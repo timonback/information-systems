6ef487cf61a5658e3fef78caf8826252a4b9a1ad
rm-bwana <r.m.bwana@student.rug.nl>
2018-01-17 12:25:56 +0100

    apriori code

--- Assignment 5 - AR/apriori.py
+++ Assignment 5 - AR/apriori.py
@@ -1,10 +1,11 @@
-import os, csv
+import csv
 from collections import defaultdict
 from itertools import chain, combinations
 
-def read_csv(filenname):
+
+def read_csv(filename):
     transactions = []
-    for item in csv.reader(open(filenname, 'r')):
+    for item in csv.reader(open(filename, 'r')):
         transactions.append([it for it in item if it not in ''])
 
     return transactions
@@ -30,37 +31,10 @@ def calc_itemsets(trans, min_sup, items, supports):
                 supports[item] += 1
 
     for item, count in local.items():
-        if float(count)/len(trans) >= min_sup:
+        if float(count) / len(trans) >= min_sup:
             curr.add(item)
 
     return curr
-
-
-def subsets(arr):
-    return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
-
-
-def calc_rules(all_itemsets, min_conf, supports, transactions):
-    toRetItems = []
-    for key, value in all_itemsets.items():
-        toRetItems.extend([(tuple(item), float(supports[item])/len(transactions))
-                           for item in value])
-
-    toRetRules = []
-    for key, value in all_itemsets.items()[1:]:
-        for item in value:
-            _subsets = map(frozenset, [x for x in subsets(item)])
-            for element in _subsets:
-                remain = item.difference(element)
-                if len(remain) > 0:
-                    c_num = float(supports[item])/len(transactions)
-                    c_denom = float(supports[element])/len(transactions)
-                    confidence = c_num/c_denom
-                    if confidence >= min_conf:
-                        toRetRules.append(((tuple(element), tuple(remain)),
-                                           confidence))
-
-    return toRetItems, toRetRules
 
 
 def run_apriori(min_supp, min_conf):
@@ -82,10 +56,27 @@ def run_apriori(min_supp, min_conf):
         curr_set = calc_itemsets(transactions, min_supp, next_set, supports)
         newset = curr_set
 
-    return calc_rules(all_itemsets, min_conf, supports, transactions)
+    rules = []
+    for k, v in all_itemsets.items():
+        for item in v:
+            possib = map(frozenset, [x for x in chain(*[combinations(item, i + 1) for i, a
+                                                        in enumerate(item)])])
+
+            for element in possib:
+                remain = item.difference(element)
+                if len(remain) > 0:
+                    c_num = float(supports[item]) / len(transactions)
+                    c_denom = float(supports[element]) / len(transactions)
+                    calc_confidence = c_num / c_denom
+                    if calc_confidence >= min_conf:
+                        rules.append(((tuple(element), tuple(remain)),
+                                      calc_confidence))
+    return rules
 
 
 if __name__ == '__main__':
-    data = run_apriori(0.001, 0.8)
-    print(data)
-
+    support = 0.001
+    confidence = 0.8
+    rules = run_apriori(support, confidence)
+    print('Print rules:' + str(len(rules)))
+    print(sorted(rules, key=lambda x: x[1], reverse=True))
